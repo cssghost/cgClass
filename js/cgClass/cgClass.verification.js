e9cc848c1ba2db3e17d46475aad50d1c76137873
cssghost <xuchen011@sina.com>
2013-10-29 17:52:47 +0800

    edit Verification

--- js/cgClass/cgClass.verification.js
+++ js/cgClass/cgClass.verification.js
@@ -2,7 +2,21 @@ cgClass.AddClass(
 	"Verification",
 	{
 		init : function(options){
-			var self = this;
+			var self = this,
+				option = $.extend({
+			        wrap : $(".wrap"),
+			        hookDom: ".Js-verification",
+			        map : "",
+			        btn : $("Js-btn"),
+			        parseAjaxData : null,
+			        successTemp : null,
+			        errorTemp : null,
+			        init : function(){},
+			        error : function(){},
+			        success : function(){},
+			        otherInput : ".other"
+                }, options);
+
 			/* do something */
 			self.outParam = self.applyMethods({
 				option : options
