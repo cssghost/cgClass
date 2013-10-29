664a4e720fc52395a5fd74b941d35a6a77670c31
cssghost <xuchen011@sina.com>
2013-10-29 16:07:06 +0800

    finish cgClass.ModulePopupLogin API doc

--- js/cgClass/module-popup-login.js
+++ js/cgClass/module-popup-login.js
@@ -1,13 +1,80 @@
+/**
+ * @author 徐晨 
+ * @name cgClass.ModulePopupLogin
+ * @class 登录弹出框 <a href="../demo/popup-login.html" target="_blank">demo</a>
+ * @constructor
+ * @extends cgClass.Popup
+ * @extends jQuery
+ * @since version 0.1 
+ * @param {Object} options 参数对象数据
+ * @param {css class} options.addClass 登陆框的样式名称
+ * @param {String} options.url 登录接口
+ * @param {String} options.dataType 回调数据类型
+ * @param {Boolean} options.autoShow 是否默认弹出
+ * @param {Function} options.callback 登录接口的回调函数
+ * @example cgClass.Create(
+	"ModulePopupLogin",
+	{
+  [可选]addClass : "module-popup-login",
+		url : "url",
+		dataType : "json",
+  [可选]autoShow : true,
+		callback : function(opt){
+			// opt为cgClass.Popop的外部提供参数
+			// 关闭方式为opt.close()
+		}
+	}
+); 
+ * @example var oLogin = cgClass.Create(
+	"ModulePopupLogin",
+	{
+  [可选]addClass : "module-popup-login",
+		url : "url",
+		dataType : "json",
+		autoShow : false,
+		callback : function(opt){
+			// opt为cgClass.Popop的外部提供参数
+			// 关闭方式为opt.hide()
+		}
+	}
+	oLogin.show();
+); 
+ */
 cgClass.AddClass(
 	"ModulePopupLogin",
 	{
 		init : function(options){
 			var self = this,
-				option = $.extend({
+				option = $.extend(/** @lends cgClass.ModulePopupLogin.prototype*/{
+					/**
+			         * 登陆框的样式名称
+			         * @type css class
+			         * @default {}
+			         */
 					addClass : "module-popup-login",
+					/**
+			         * 登录接口
+			         * @type String
+			         * @default {}
+			         */
 					url : "url",
+					/**
+			         * 回调数据类型
+			         * @type String
+			         * @default {}
+			         */
 					dataType : "json",
+					/**
+			         * 是否默认弹出
+			         * @type Boolean
+			         * @default {}
+			         */
 					autoShow : true,
+					/**
+			         * 登录接口的回调函数
+			         * @type Function
+			         * @default null
+			         */
 					callback : null
 				}, options),
 				loginPopupCon = '<div class="text Js-name"><input type="text" placeholder="请输入用户名" /><p class="error">请输入用户名</p></div>'+
@@ -68,6 +135,11 @@ cgClass.AddClass(
 				}
 			);
 		},
+		/**
+		 * @name cgClass.ModulePopupLogin#testLoginNull
+		 * @desc  验证输入框是否为空
+		 * @event
+		 */
 		testLoginNull : function($text){
 			if ( $.trim( $text.val() ) == "" ) {
 				$text.siblings(".error").slideDown("fast").closest(".text").addClass("text-error");
@@ -75,6 +147,11 @@ cgClass.AddClass(
 				$text.siblings(".error").slideUp("fast").closest(".text").removeClass("text-error");
 			}
 		},
+		/**
+		 * @name cgClass.ModulePopupLogin#show
+		 * @desc  显示弹出框
+		 * @event
+		 */
 		show : function(){
 			this.popup.reset();
 			this.popup.show();
